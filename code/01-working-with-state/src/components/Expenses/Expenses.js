5c9b4130debe4c293664b9e058db6f17198c364a
kaitingp <kaiting.peck@gmail.com>
2023-08-20 21:26:48 +0800

    half of section 6

--- code/01-working-with-state/src/components/Expenses/Expenses.js
+++ code/01-working-with-state/src/components/Expenses/Expenses.js
@@ -6,12 +6,28 @@ import Card from "../UI/Card";
 import "./Expenses.css";
 
 const Expenses = (props) => {
-  const [selectedYear, setSelectedYear] = useState("2020");
+  const [selectedYear, setSelectedYear] = useState(2020);
 
   const selectedYearHandler = (year) => {
-    setSelectedYear(year);
-    console.log(year);
+    setSelectedYear(parseInt(year));
   };
+
+  const filteredExpenses = props.items.filter(
+    (item) => item.date.getFullYear() === selectedYear
+  );
+
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((item) => (
+      <ExpenseItem
+        key={item.id}
+        title={item.title}
+        amount={item.amount}
+        date={item.date}
+      />
+    ));
+  }
 
   return (
     <div>
@@ -20,13 +36,17 @@ const Expenses = (props) => {
           onSelectedYearChanged={selectedYearHandler}
           selected={selectedYear}
         />
-        {props.items.map((item) => (
-          <ExpenseItem
-            title={item.title}
-            amount={item.amount}
-            date={item.date}
-          />
-        ))}
+        {/* {filteredExpenses.length === 0 && <p>No expenses found</p>}
+        {filteredExpenses > 0 &&
+          filteredExpenses.map((item) => (
+            <ExpenseItem
+              key={item.id}
+              title={item.title}
+              amount={item.amount}
+              date={item.date}
+            />
+          ))} */}
+        {expensesContent}
       </Card>
     </div>
   );
